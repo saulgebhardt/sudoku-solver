ac2f9d76cb23c61b797c1e14f202d0326019210a
Saul Gebhardt <saul.gebhardt@tiscali.nl>
2024-05-23 09:34:19 +0200

    Create outline of project, including what functions have to be (roughly) created to solve the given sudoku

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
+    //TODO: Read a single sudoku from a file, and parse it as a 2d array(Vec) of u8
+    let _temp_string = String::from(
+        "050703060007000800000816000000030000005000100730040086906000204840572093000409000",
+    );
     println!("Hello, world!");
 }
+
+//TODO: Implement a sudoku solving algorithm. Backtracking seems like a decent start
+//
+//
+//TODO: Print sudoku to console cleanly, think if we need the bars, what values look like, et
+//cetera.
+//
+//
+//TODO: Implement some small timing tool, useful for assessing efficiency of algorithm and show
+//paralellism.
